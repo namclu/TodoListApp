5d1e255eb0e68089a8147406af6483a14d2f8882
Nam Lu <namlu1205@gmail.com>
2019-09-13 14:32:52 +1000

    Set text for new and existing Todo details

--- app/src/main/java/com/namlu/todolistapp/TodoDetailsActivity.kt
+++ app/src/main/java/com/namlu/todolistapp/TodoDetailsActivity.kt
@@ -1,14 +1,22 @@
 package com.namlu.todolistapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.namlu.todolistapp.models.Todo
 import com.namlu.todolistapp.util.Constants
 
 class TodoDetailsActivity : AppCompatActivity() {
 
-    companion object{
+    private lateinit var editTitle: EditText
+    private lateinit var textTitle: TextView
+    private lateinit var editContent: EditText
+
+    private lateinit var todo: Todo
+    private var isNewTodo = false
+
+    companion object {
         const val TAG = "TodoDetailsActivity"
     }
 
@@ -16,9 +24,42 @@ class TodoDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo_details)
 
-        if (intent.hasExtra(Constants.SELECTED_TODO_KEY)) {
-            val todo = intent.getParcelableExtra<Todo>(Constants.SELECTED_TODO_KEY)
-            Log.d(TAG, "onCreate() $todo")
+        editTitle = findViewById(R.id.edit_todo_details_title)
+        textTitle = findViewById(R.id.text_todo_details_title)
+        editContent = findViewById(R.id.edit_todo_details)
+
+        if (isNewTodo()) {
+            // New item so enter Edit mode
+            setNewTodoProperties()
+        } else {
+            // Not a new item so enter View mode
+            setTodoProperties()
         }
+    }
+
+    private fun isNewTodo(): Boolean {
+        if (intent.hasExtra(Constants.SELECTED_TODO_KEY)) {
+            // If item is not new, get its data
+            todo = intent.getParcelableExtra(Constants.SELECTED_TODO_KEY)
+
+            isNewTodo = false
+        } else {
+            isNewTodo = true
+        }
+
+        return isNewTodo
+    }
+
+    // Set text for a new item
+    private fun setNewTodoProperties() {
+        editTitle.setText(R.string.new_todo)
+        textTitle.text = getString(R.string.new_todo)
+    }
+
+    // Set text for a
+    private fun setTodoProperties() {
+        editTitle.setText(todo.title)
+        textTitle.text = todo.title
+        editContent.setText(todo.content)
     }
 }
