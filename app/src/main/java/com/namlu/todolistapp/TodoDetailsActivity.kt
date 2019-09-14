60a78ada53e3744bbaf225770001fa18faac220f
Nam Lu <namlu1205@gmail.com>
2019-09-14 15:56:15 +1000

    Add ability to respond to double tap

--- app/src/main/java/com/namlu/todolistapp/TodoDetailsActivity.kt
+++ app/src/main/java/com/namlu/todolistapp/TodoDetailsActivity.kt
@@ -1,13 +1,20 @@
 package com.namlu.todolistapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.namlu.todolistapp.models.Todo
 import com.namlu.todolistapp.util.Constants
 
-class TodoDetailsActivity : AppCompatActivity() {
+class TodoDetailsActivity : AppCompatActivity(),
+    View.OnTouchListener,
+    GestureDetector.OnGestureListener,
+    GestureDetector.OnDoubleTapListener {
 
     private lateinit var editTitle: EditText
     private lateinit var textTitle: TextView
@@ -15,6 +22,7 @@ class TodoDetailsActivity : AppCompatActivity() {
 
     private lateinit var todo: Todo
     private var isNewTodo = false
+    private lateinit var gestureDetector: GestureDetector
 
     companion object {
         const val TAG = "TodoDetailsActivity"
@@ -24,10 +32,18 @@ class TodoDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo_details)
 
+        // Init views
         editTitle = findViewById(R.id.edit_todo_details_title)
         textTitle = findViewById(R.id.text_todo_details_title)
         editContent = findViewById(R.id.edit_todo_details)
 
+        // Init gesture detection
+        gestureDetector = GestureDetector(this, this)
+
+        // Listen for a DoubleTap event
+        editContent.setOnTouchListener(this)
+
+        // Init an item
         if (isNewTodo()) {
             // New item, enter Edit mode
             setNewTodoProperties()
@@ -35,6 +51,48 @@ class TodoDetailsActivity : AppCompatActivity() {
             // Existing item, enter View mode
             setTodoProperties()
         }
+    }
+
+    // Callback for View.OnTouchListener
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        return gestureDetector.onTouchEvent(event)
+    }
+
+    // Callbacks for GestureDetector.OnGestureListener
+    override fun onShowPress(e: MotionEvent?) {
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+        return false
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+    }
+
+    // Callbacks for GestureDetector.OnDoubleTapListener
+    override fun onDoubleTap(e: MotionEvent?): Boolean {
+        Log.d(TAG, "onDoubleTap() fired")
+        return true
+    }
+
+    override fun onDoubleTapEvent(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+        return false
     }
 
     // Checks if we're looking at a new or existing item and sets the flag
