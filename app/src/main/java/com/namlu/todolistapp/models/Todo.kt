f5cc620c2b7d8d00e36d153466ced33fec297100
Nam Lu <namlu1205@gmail.com>
2019-09-16 02:22:56 +1000

    Add database entities

--- app/src/main/java/com/namlu/todolistapp/models/Todo.kt
+++ app/src/main/java/com/namlu/todolistapp/models/Todo.kt
@@ -2,26 +2,37 @@ package com.namlu.todolistapp.models
 
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "todo_table")
 data class Todo(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    @ColumnInfo(name = "title")
     var title: String?,
+    @ColumnInfo(name = "content")
     var content: String?,
+    @ColumnInfo(name = "timestamp")
     var timestamp: String?
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
+        parcel.readInt(),
         parcel.readString(),
         parcel.readString(),
         parcel.readString()
     )
 
-    override fun writeToParcel(parcel: Parcel?, flags: Int) {
-        parcel?.writeString(title)
-        parcel?.writeString(content)
-        parcel?.writeString(timestamp)
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(title)
+        parcel.writeString(content)
+        parcel.writeString(timestamp)
     }
 
     override fun describeContents(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 0
     }
 
     companion object CREATOR : Parcelable.Creator<Todo> {
